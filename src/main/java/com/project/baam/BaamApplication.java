cd868a78208ba85ececcd4d162b75afd4156ceea
DESKTOP-LHRAOAP\user <krvahn@ppfriends.co.kr>
2023-09-21 00:30:26 +0900

    init

---
+++ src/main/java/com/project/baam/BaamApplication.java
@@ -0,0 +1,13 @@
+package com.project.baam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/baam/BaamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.baam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
